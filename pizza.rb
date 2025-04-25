575964409d058c7af236d1a4f4bedcb878eaad5d
KTilted <gmasikmasikmasik@gmail.com>
2025-04-25 15:18:03 +0300

    Update pizza.rb

--- pizza.rb
+++ pizza.rb
@@ -2,7 +2,7 @@ class Pizza < Formula
   desc "🍕 The Пицца esoteric language interpreter"
   homepage "https://github.com/ktilted/pizza"
   url "https://github.com/KTilted/pizza/releases/download/1.1/pizza.tar.gz"  # Архив с бинарником
-  sha256 "6b8a44e43dde9a2709205966a9f92f46f666a9b1f20dd2f28b9d607a3af224be"
+  sha256 "6d6919ae53c9553f5be0f252ec5e61d1a0ba4e58a0c74f36db50a7e50629b393"
   license "MIT"
 
   def install
